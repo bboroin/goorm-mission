b699d535b117485c271408f15a8ebbe01cb49078
로인 <151013952+bboroin@users.noreply.github.com>
2025-11-18 14:56:23 +0900

    feat: todo 목록 조회 및 로딩/에러 상태 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  const fetchTodos = async () => {
+    const res = await axios.get("http://localhost:4000/todos");
+    return res.data;
+  };
+
+  const {
+    data: todos = [],
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
+
+  if (isPending) return <div>로딩 중...</div>;
+  if (isError) return <div>데이터를 불러오지 못했습니다.</div>;
+
+  return (
+    <section>
+      <header>
+        <h2>Todo List - Tanstack Query</h2>
+      </header>
+
+      <ul className="list">
+        {todos.map((todo) => (
+          <li key={todo.id} className="item">
+            <div className="title">{todo.title}</div>
+            <p>{todo.completed ? "완료" : "진행 중"}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default App;
