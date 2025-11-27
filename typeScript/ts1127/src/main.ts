a509d3cebffae68fd04c8ed03933ad5a18bbcf69
로인 <151013952+bboroin@users.noreply.github.com>
2025-11-27 14:58:01 +0900

    feat: JSONPlaceholder API 타입 정의 추가 (Post, User) #5

---
+++ typeScript/ts1127/src/main.ts
@@ -0,0 +1,31 @@
+// 1. User, Post 타입 정의
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
