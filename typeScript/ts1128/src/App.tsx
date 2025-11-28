007a081114c19159cb58c7ec2a14f6fbb0b4f55a
로인 <151013952+bboroin@users.noreply.github.com>
2025-11-28 16:39:29 +0900

    feat: fetch로 Todo 데이터 불러오는 기능 구현 #7

--- typeScript/ts1128/src/App.tsx
+++ typeScript/ts1128/src/App.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import type { Todo } from "./types/todo";
+
+const BASE_URL = "http://localhost:4000/todos";
 
 function App() {
-  return <></>;
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const getTodos = async (): Promise<Todo[]> => {
+    const res = await fetch(BASE_URL);
+    if (!res.ok) {
+      throw new Error("Failed to fetch todos");
+    }
+    return res.json();
+  };
+
+  useEffect(() => {
+    getTodos().then((data) => setTodoList(data));
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
