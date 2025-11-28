ff79763808d5cb204008a5b410284acd100b794f
로인 <151013952+bboroin@users.noreply.github.com>
2025-11-28 16:46:35 +0900

    feat: Todo 입력 상태 관리와 onChange 핸들러 구현 #7

--- typeScript/ts1128/src/App.tsx
+++ typeScript/ts1128/src/App.tsx
@@ -6,6 +6,7 @@ const BASE_URL = "http://localhost:4000/todos";
 
 function App() {
   const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [title, setTitle] = useState("");
 
   const getTodos = async (): Promise<Todo[]> => {
     const res = await fetch(BASE_URL);
@@ -19,9 +20,24 @@ function App() {
     getTodos().then((data) => setTodoList(data));
   }, []);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+    console.log("입력값 : ", e.target.value);
+  };
+
   return (
     <div>
       <h2 className="header">TS + React TodoList</h2>
+
+      <div className="form">
+        <input
+          className="input"
+          type="text"
+          value={title}
+          onChange={handleChange}
+        />
+        <button>등록</button>
+      </div>
 
       <ul className="list">
         {todoList.map((todo) => (
